ad9a05048c5f9a9b70dea3d4348621dbbb336a92
Prateek Singh <47247309+prateekLambda@users.noreply.github.com>
2026-09-09 19:14:30 +0530

    Update test_1.spec.js

--- tests/test_1.spec.js
+++ tests/test_1.spec.js
@@ -13,7 +13,7 @@ test.describe('PlayWright Vanilla JS - 1', () => {
     await page.click('text=Test Generator');
     await page.click('text=Release notes');
     await page.click('text=Annotations');
-    await page.click('');
+    //await page.click('');
     await page.click('text=Authentication');
     await page.click('text=Command line');
     await page.click('text=Configuration');
